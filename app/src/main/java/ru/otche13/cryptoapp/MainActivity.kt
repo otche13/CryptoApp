b09d72c21263f2e1343a895fbefa8703eecc0de5
Sergei <97453764+otche13@users.noreply.github.com>
2022-09-30 22:05:04 +0300

    Первый пункт README ~2ч 43

--- app/src/main/java/ru/otche13/cryptoapp/MainActivity.kt
+++ app/src/main/java/ru/otche13/cryptoapp/MainActivity.kt
@@ -2,10 +2,36 @@ package ru.otche13.cryptoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import ru.otche13.cryptoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navHostFragment: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment?
+            ?: return
+
+        with(navHostFragment.navController) {
+
+            setupActionBarWithNavController(this )
+        }
     }
+
+
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        navHostFragment.navController.navigateUp()
+        return super.onSupportNavigateUp()
+    }
+
 }
